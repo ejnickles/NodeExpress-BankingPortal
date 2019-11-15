9cb41d91ce910256a866f6f20268b8dff9e9db81
Vegeta <ejn@nicklesbuilding.com>
2019-11-15 18:50:45 -0500

    mod3

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.set( 'views', path.join( __dirname, 'views' ));
 app.set( 'view engine', 'ejs' );
 
 app.use( express.static( path.join( __dirname, 'public' )));
+app.use(express.urlencoded({extended: true}))
 
 const accountData = fs.readFileSync(
   path.join( __dirname, 'json', 'accounts.json' ), 'utf8'
@@ -24,5 +25,25 @@ app.get( '/savings', ( req, res ) => res.render( 'account', { account: accounts.
 app.get( '/checking', ( req, res ) => res.render( 'account', { account: accounts.checking } ));
 app.get( '/credit', ( req, res ) => res.render( 'account', { account: accounts.credit } ));
 app.get( '/profile', ( req, res ) => res.render( 'profile', { user: users[0] } ));
+app.get( '/transfer', ( req, res ) => res.render( 'transfer', {}));
+app.post( '/transfer', ( req, res ) => {
+  accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+  accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount);
+  const accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(
+    path.join( __dirname, 'json', 'accounts.json' ), accountsJSON, 'utf8'
+  );
+  res.render( 'transfer', { message: "Transfer Completed" });
+});
+app.get( '/payment', ( req, res ) => res.render( 'payment', { account: accounts.credit }));
+app.post( '/payment', ( req, res ) => {
+  accounts.credit.balance = accounts.credit.balance - req.body.amount;
+  accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount);
+  const accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(
+    path.join( __dirname, 'json', 'accounts.json' ), accountsJSON, 'utf8'
+  );
+  res.render( 'payment', { message: "Payment Successful", account: accounts.credit })
+});
 
 app.listen( 3000, () => console.log( 'PS Project Running on port 3000!' ));
