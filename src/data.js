b17f25dc81e91b5b4cdcbffa098d029b97f057fb
Vegeta <ejn@nicklesbuilding.com>
2020-06-06 09:58:43 -0400

    4

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const accountData = fs.readFileSync(
+  path.join( __dirname, 'json', 'accounts.json' ), 'utf8'
+);
+const accounts = JSON.parse( accountData );
+
+const userData = fs.readFileSync(
+  path.join( __dirname, 'json', 'users.json' ), 'utf8'
+);
+const users = JSON.parse( userData );
+
+const writeJSON = act => {
+  fs.writeFileSync(
+    path.join( __dirname, 'json', 'accounts.json' ), act, 'utf8'
+  );
+}
+
+module.exports = {
+  accounts,
+  users,
+  writeJSON,
+}
